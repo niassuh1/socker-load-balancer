edf9710ebc3724163920d3aa129fcb8ae450ad0b
niassuh1 <galenwaros@gmail.com>
2024-01-30 18:46:47 +0300

    refactor: removed dist folder

--- dist/redis/redis-client.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.publisher = exports.subscriber = void 0;
-const redis_1 = require("redis");
-exports.subscriber = (0, redis_1.createClient)({ url: `redis://localhost:6379/` });
-exports.publisher = (0, redis_1.createClient)({ url: `redis://localhost:6379/` });
